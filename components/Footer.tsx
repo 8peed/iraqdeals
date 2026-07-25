a0977b57a33c18ea04c7704884eb884238118027
8peed <rico69sink@gmail.com>
2026-07-25 15:05:18 +0300

    Add Footer component with copyright notice

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="border-t border-border bg-white mt-20 py-10">
+      <div className="max-w-6xl mx-auto px-6 text-center text-sm text-textlow">
+        © 2026 عراق ديلز. جميع الحقوق محفوظة.
+      </div>
+    </footer>
+  );
+}
